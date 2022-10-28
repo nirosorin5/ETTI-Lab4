75c46a8297f65ce37cd0bebee71ece61e111b68a
nirosorin5 <94898661+nirosorin5@users.noreply.github.com>
2022-10-28 10:54:05 +0300

    Add files via upload

---
+++ Proiect_1.py
@@ -0,0 +1,6 @@
+
+Nume = input("Introdu numele tau: ")
+Varsta = int(input("Introdu varsta ta: "))
+data = 2022
+p1 = str(2022 - Varsta + 100)
+print(Nume + ", anul în care o sa ai 100 de ani este " + p1)
